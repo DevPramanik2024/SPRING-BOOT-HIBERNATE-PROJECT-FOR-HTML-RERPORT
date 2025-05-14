c22be6712f9ea620bc1294697fc6c8492c4e2e27
DevPramanik2024 <kdev80170@gmail.com>
2025-05-14 16:15:53 +0530

    first commit

--- src/main/java/com/ninja/springboot_java_hibernate_test/EmployeeController.java
+++ src/main/java/com/ninja/springboot_java_hibernate_test/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.ninja.springboot_java_hibernate_test;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,4 +27,16 @@ public class EmployeeController
 		erepo.save(employee);
 		return "data saved into table";
 	}
+	
+@RequestMapping("/all")
+public List<Employee> alldata()
+{
+	return erepo.findAll();
+			
+}
+@RequestMapping("/by/{name}")
+public List<Employee> byname(@PathVariable String name)
+{
+	return erepo.findByName(name);
+}
 }
